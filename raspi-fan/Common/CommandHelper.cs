fc7bf75041e07d9051e1051b64d9b0d7c9585edd
John <nowyoutry@outlook.com>
2021-11-12 10:33:19 +0800

    Add project files.

---
+++ raspi-fan/Common/CommandHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+public static class CommandHelper
+{
+    // https://stackoverflow.com/questions/40764172/how-asp-net-core-execute-linux-shell-command
+    public static string Execute(string command, string args)
+    {
+        var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = command,
+                Arguments = args,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            }
+        };
+        process.Start();
+        string output = process.StandardOutput.ReadToEnd();
+        string error = process.StandardError.ReadToEnd();
+        process.WaitForExit();
+
+        if (string.IsNullOrEmpty(error)) { return output; }
+        else { return error; }
+    }
+}
---
+++ raspi-fan/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Device.Gpio;
+
+
+
+
+int fanGPIO = 14;
+
+using GpioController controller = new();
+controller.OpenPin(fanGPIO);
+
+
+while (true)
+{
+    var temp = GetTemperature();
+    Console.WriteLine($"CPU Temperature : {temp}");
+    var pinMode = controller.GetPinMode(fanGPIO);
+    Console.WriteLine($"Current PinMode : {pinMode}");
+    ControlFan(50, temp);
+    await Task.Delay(1000 * 60);
+}
+
+//Console.WriteLine("Hello, World!");
+
+static double GetTemperature()
+{
+    string result = CommandHelper.Execute("cat", "/sys/class/thermal/thermal_zone0/temp");
+    double.TryParse(result, out double temperature);
+    return temperature / 1000;
+}
+
+void ControlFan(double threshold, double temperature)
+{
+    PinMode pinMode = temperature >= threshold ? PinMode.Input : PinMode.Output;
+    Console.WriteLine($"Setting pin mode to {pinMode}");
+    controller.SetPinMode(fanGPIO, pinMode);
+}
